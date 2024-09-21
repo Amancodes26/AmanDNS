501957d44f92c7207fed27a778c0ec899c37c333
Aman <amansngh8586@gmail.com>
2024-09-21 12:06:16 +0530

    something

---
+++ app/dns/header.ts
@@ -0,0 +1,68 @@
+export enum OPCODE{
+    STANDARD_QUERY = 0,
+    INVERSE_QUERY = 1,
+    SERVER_STATUS_REQUEST = 2,
+    RESERVED = 3,
+}
+
+export enum ResponseCode{
+    NO_ERROR = 0,
+    FORMAT_ERROR = 1,
+    SERVER_FAILURE =2,
+    NAME_ERROR=3,
+    NOT_IMPLEMENTED=4,
+    REFUSED=5,
+}
+
+export enum QueryType{
+    A = 1,
+    NS = 2,
+    CNAME = 5,
+    SOA = 6,
+    PTR = 12,
+    MX = 15,
+    TXT = 16,
+    AAAA = 28,
+}
+
+export enum QueryClass{
+    IN = 1,
+    CH = 3,
+    HS = 4,
+}
+
+export interface TDNSHeader{
+    ID:number,
+    QR: number,
+    OPCODE: OPCODE,
+    AA:boolean,
+    TC:boolean,
+    RD:boolean,
+    RA:boolean,
+    Z:number,
+    ResponseCode: ResponseCode,
+    QDCount:number,
+    ANCount:number,
+    NSCount:number,
+    ARCount:number,
+}
+
+class DNSHeader{
+    static write(values: TDNSHeader){
+        const header = Buffer.alloc(12);
+
+        const flags = values.QR | (values.AA ? 1 : 0) << 5 | (values.TC ? 1 : 0) << 6 | (values.RD ? 1 : 0) << 7 | (values.RA ? 1 : 0) << 8 | values.Z << 4 | values.ResponseCode;
+        header.writeUInt16BE(values.ID, 0);
+        header.writeUInt16BE(flags, 2);
+        header.writeUInt16BE(values.QDCount, 4);
+        header.writeUInt16BE(values.ANCount, 6);
+        header.writeUInt16BE(values.NSCount, 8);
+        header.writeUInt16BE(values.ARCount, 10);
+
+        return header;
+    }
+    
+}
+
+export default DNSHeader;
+
--- app/main.ts
+++ app/main.ts
@@ -1,17 +1,36 @@
 import * as dgram from "dgram";
+import DNSHeader ,{OPCODE, ResponseCode,TDNSHeader} from "./dns/header" ;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+const defaultHeader: TDNSHeader = {
+    ID: 1234,
+    QR: 1,
+    OPCODE: OPCODE.STANDARD_QUERY,
+    AA: false,
+    TC: false,
+    RD: false,
+    RA: false,
+    Z: 0,
+    ResponseCode: ResponseCode.NO_ERROR,
+    QDCount: 0,
+    ANCount: 0,
+    NSCount: 0,
+    ARCount: 0,
+}
+
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-//
+// Create a UDP server
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
 
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
+        
+
+        const header = DNSHeader.write(defaultHeader);
+        const response = Buffer.concat([header, data]);
+
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
