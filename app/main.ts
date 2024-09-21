0945306fdda508accd7ed576b50997ae5fefc212
Aman <amansngh8586@gmail.com>
2024-09-22 02:38:42 +0530

    something

--- app/main.ts
+++ app/main.ts
@@ -43,6 +43,8 @@ function createResponseHeader(requestHeader: TDNSHeader, answerCount: number): T
     // Set RCODE to 4 (Not Implemented) if the query type is not supported
     if (requestHeader.QDCount === 0 || answerCount === 0) {
         responseCode = 4; // Not Implemented
+    } else {
+        responseCode = 0; // NOERROR
     }
 
     return {
